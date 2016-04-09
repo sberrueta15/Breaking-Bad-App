5d26b891da298ed3fff206f061a836846198fa88
SP26 <SP26@SP26.local>
2016-04-08 22:28:40 -0300

    commit de app tableView

---
+++ AppTableView/AppTableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  AppTableView
+//
+//  Created by SP26 on 8/4/16.
+//  Copyright © 2016 SP26. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
